47921fa4154698286a23ccc7a93faed672dda77b
Silas Augusto da Cunha <0997s.a.cunha@gmail.com>
2024-10-06 21:45:30 -0300

    Iniciando excercicios 2 pagina 36, tentei usar OOP para solucionar o excercicio

--- excercicio2/Program.cs
+++ excercicio2/Program.cs
@@ -1,2 +1,47 @@
-﻿// See https://aka.ms/new-console-template for more information
-Console.WriteLine("Hello, World!");
+﻿/*
+  Excercicios:36.
+    Calcular a quantidade litros de combustível gasta em uma viagem, utilizando um automoval que faz 12km por litro.
+  para  um cálculo, o usuario deve fornecer o tempo gasto na viagem e a velocidade média. Dessa forma, é possivel obter
+  a distância percorrida com a fórmula DISTANCIA=TEMPO*VELOCIDADE. Tendo o valor da distancia, basta calcular a quantidade
+  de litros de combústivel utilizada na viagem com a fórmu LITROS_USADOS = DISTANCIA/12. O Programa deve apresentar os valores
+  da velocidade média, o tempo gasto, a distância percorrida e a quantidade litros utilizados na viagem. Trabalhe com valores reais.
+  
+  Tipos de dados inteiros:
+  Double,
+  Float,
+  Long,
+  Decimal.
+  */
+
+using System;
+using System.Collections.Generic;
+
+public class programa
+{
+    public static void Main()
+    {
+        System.Console.WriteLine("Olá Mundo!");
+        Double viagem_primary = new Double();
+        System.Console.WriteLine(viagem_primary);
+    }
+}
+
+public class Carro
+{
+  public Double DISTANCIA, LITROS;
+  public Double Tempo = 12;
+  
+  public Double bourn_power(){
+    Console.WriteLine("Digite o tempo gasto na distancia: ");
+    Double TEMP_DIST = Parse(Console.ReadLine());
+
+    Console.WriteLine("Digite o tempo gasto na distancia: ");
+    Double VELOCIDADE = Parse(Console.ReadLine());
+
+    Double COMB_GAST=TEMPO*VELOCIDADE;
+
+
+
+    return COMB_GAST;
+  }
+}
